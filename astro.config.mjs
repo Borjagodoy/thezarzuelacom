580692ff743d85d0d886615d8c9f2b70f9d53389
Borja-godoy <borja.godoy@ibm.com>
2025-08-02 13:39:33 +0200

    fix render

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: "server", // NO debe ser "static" si usas SSR
+
+  // NO debe ser "static" si usas SSR
+  output: "server",
+
+  adapter: vercel()
 });
